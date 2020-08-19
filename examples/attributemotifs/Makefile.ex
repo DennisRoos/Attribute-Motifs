2b25f829c2fbc58a8c600f256763120d52bf6d7e
DennisRoos <dennis.roos0@gmail.com>
2020-08-19 06:02:42 +0200

    Add files via upload

---
+++ examples/attributemotifs/Makefile.ex
@@ -0,0 +1,7 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = attributemotifsmain
+DEPH = $(EXSNAPADV)/attributemotifs.h
+DEPCPP = $(EXSNAPADV)/attributemotifs.cpp
